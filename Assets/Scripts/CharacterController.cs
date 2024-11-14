8e6992b9c58f5a773b3c30ca2d1f81c459ca71b5
Joel Ramos <joelramos@ceroca.cat>
2024-11-14 17:53:16 +0100

    Nivel1 acabado + Nivel 2

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,25 +5,53 @@ using UnityEngine;
 public class CharacteController : MonoBehaviour
 {
     public float velocidad;
+    public float fuerzaSalto;
     private Rigidbody2D rigidBody;
+    public float saltosMaximos;
     private bool mirandoDerecha = true;
+    private BoxCollider2D boxCollider;
+    public LayerMask capaSuelo;
+    private float saltosRestantes;
+
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        saltosRestantes = saltosMaximos; 
     }
+
     void Update()
     {
         ProcesarMovimiento();
+        ProcesarSalto();
     }
-    // Update is called once per frame
+
     void ProcesarMovimiento()
     {
-       float inputMovimiento = Input.GetAxis("Horizontal");
-        GetComponent<Rigidbody2D>().velocity = new Vector2(inputMovimiento * velocidad, GetComponent<Rigidbody2D>().velocity.y);
+        float inputMovimiento = Input.GetAxis("Horizontal");
+        rigidBody.velocity = new Vector2(inputMovimiento * velocidad, rigidBody.velocity.y);
 
         GestionarOrientacio(inputMovimiento);
-
     }
+
+    bool EstaEnSuelo()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, 0.2f, capaSuelo);
+        return raycastHit.collider != null;
+    }
+
+    void ProcesarSalto()
+    {
+        if (EstaEnSuelo()){
+            saltosRestantes = saltosMaximos;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && saltosRestantes > 0)
+        {
+            saltosRestantes--;
+            rigidBody.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+        }
+    }
+
     void GestionarOrientacio(float inputMovimiento)
     {
         if (mirandoDerecha == true && inputMovimiento < 0 || mirandoDerecha == false && inputMovimiento > 0)
@@ -31,6 +59,6 @@ public class CharacteController : MonoBehaviour
             mirandoDerecha = !mirandoDerecha;
             transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
         }
-
     }
 }
+
