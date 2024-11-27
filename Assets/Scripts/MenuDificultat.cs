2c19f0be7ccca6db7e5b8d05f448c2971064cb7d
Joel Ramos <joelramos@ceroca.cat>
2024-11-27 16:13:11 +0100

    Cambios menu

--- Assets/Scripts/MenuDificultat.cs
+++ Assets/Scripts/MenuDificultat.cs
@@ -3,11 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class MenuDificultad : MonoBehaviour
 {
-    public void SeleccionarDificultad(string dificultad)
-    {
-        // Asignar la dificultad seleccionada al GameManager
-        GameManager.instancia.SeleccionarDificultad(dificultad); // Llama al método de GameManager
-    }
 
     public void tornaAtras(){
         SceneManager.LoadScene("SampleScene");  // Cambia "Dificultat" por el nombre de tu escena de selección de dificultad
